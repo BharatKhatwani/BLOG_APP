750efc4d0869fca0eea14c1460227ac774412611
Bharat Khatwani <bharatkhatwani06@gmail.com>
2025-07-04 09:13:07 +0000

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function MyComponent() {
+  return (
+    <Button className="bg-blue-600 ">
+      Click Me
+    </Button>
+  );
+}
